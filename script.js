8b6b014e58e3439556887a82850753260142fd12
Billy Dominique <billydominique@pursuit.org>
2021-10-14 16:46:10 -0400

    added js code for user review submission so it displays their review and the title of the movie in bold when the form is submitted

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
 .then((films) => {
     console.log(films)
 
-    const movieSelection =document.querySelector("#movie-selection")
+    const movieSelection = document.querySelector("#movie-selection")
+    const movieTitle = document.querySelector("#movie-title")
     const releaseYear = document.querySelector("#release-year")
     const movieDescription = document.querySelector("#movie-description")
 
@@ -15,18 +16,35 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
     });
 
     document.querySelector("#movie-selection").addEventListener("change", (event) => {
+        event.preventDefault();
+
         console.log(event.target.value)
         films.forEach(film => {
-
-        if(event.target.value === film.title){    
-            
-            releaseYear.textContent = film.release_date
-            movieDescription.textContent = film.description 
+            if(event.target.value === film.title){    
+                
+                movieTitle.textContent = event.target.value
+                releaseYear.textContent = film.release_date
+                movieDescription.textContent = film.description 
             }
-
             
         });
         
+    })
+    
+    document.querySelector("form").addEventListener("submit",(event) =>{
+        event.preventDefault();
+        
+        const userReview = (event.target.review.value)
+        const reviewsList = document.querySelector("ul")
+
+        li = document.createElement("li")
+        strong = document.createElement("strong")
+        strong.textContent = movieSelection.value + ":"
+        li.textContent = " " + userReview
+        li.prepend(strong)
+        reviewsList.append(li)
+        
+        event.target.reset()
     })
 
 })
